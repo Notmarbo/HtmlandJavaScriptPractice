da39ce77d10b60d57742bc7d89ba86e3ebaf7501
Marco Sanchez <Notmarcosan@gmail.com>
2023-04-27 15:42:27 -0400

    Added JS greeting

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const greeting = document.getElementById("greeting");
+const nameTextBox = document.getElementById("nameTextBox");
+const pressMeButton = document.getElementById("pressMeButton");
+
+
+
+function init() {
+    pressMeButton.onclick = onPressMeButtonClicked;
+
+}
+window.onload = init;
+
+
+function onPressMeButtonClicked() {
+    let username = nameTextBox.value;
+    greeting.innerHTML= `Hello to ` + username;
+ 
+}
